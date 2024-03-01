2ed35445746135ff543a5cac5944c3bc025c0966
Neil Fisher <nfisher@summit-tech.ca>
2024-03-01 12:39:55 -0500

    Adjust drive pid constants

--- constants.py
+++ constants.py
@@ -50,9 +50,9 @@ class Drivetrain:
     k_dt_current_limit = 60
 
     # TODO: Tune
-    k_forward_p = 0.012
+    k_forward_p = 0.016
     k_forward_i = 0.0
-    k_forward_d = 0.0005
+    k_forward_d = 0.001
 
     k_turn_p = 0.012
     k_turn_i = 0.0
